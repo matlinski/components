feaac114eed100b0f2793d060379a0755df5d159
Mateusz <mateusz@matlinski.pl>
2019-07-05 20:39:48 +0200

    +

--- page.php
+++ page.php
@@ -24,8 +24,8 @@
 </script>
 </head>
 <body>
-<?= Button(["tag"=>"button", "dropdown"=>"<a href=\"https://google.com\">The popover works</a>"]) ?>
-
+<?php # Button(["tag"=>"button", "popover"=>["<h5>title</h5>", "right"]]) ?>
+<?= Card()?>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
--- types/Media.constructor.php
+++ types/Media.constructor.php
@@ -1,24 +1,19 @@
 <?php
 
-function Card($input = ""){
+function Media($input = ""){
     #USER INPUT ABOVE#
 $compiler = "";
-$base_class = "card";
-$default = ["content"=>"<h5>Title placeholder</h5><p>Body placeholder</p><button class='btn btn-primary'>Click here</button>", "image"=>"<img src='https://picsum.photos/286/180' alt='...'>", "width"=>"18rem", "template" =>"", "attr" => "", "style"=> "", "script"=> ""];
+$base_class = "Media";
+$default = ["content"=>"<h5>Title placeholder</h5><p>Body placeholder</p><button class='btn btn-primary'>Click here</button>", "image"=>'<img src="https://picsum.photos/286/180" class="card-img-top" alt="...">', "width"=>"18rem", "template" =>"", "attr" => "", "style"=> "", "script"=> ""];
     #PRESETS ABOVE#
 foreach(Component($input, $default, $base_class) as $key => $value) $$key = $value;
     #DATA SUPPLY ABOVE#
 $compiler .= '<span id="'.$id.'">';
-$compiler .= '<div class="'.$base_class.'" style="width: '.$width.';">';
+$compiler .= '<div class="'.$base_class.'">';
     #COMPILATION BEGINS#               
 $compiler .= $image;
 $compiler .= '<div class="card-body">'.$content.'</div>';
 // $style .= $id.'>.card>.card-body'
-$style .= '#'.$id.'>.card>img{
-    width: 100%;
-    border-top-left-radius: calc(.25rem - 1px);
-    border-top-right-radius: calc(.25rem - 1px);
-}';
 if($script) $compiler .= "<script>$script</script>";
 if($style) $compiler .= "<style>$style</style>";
     #OPTIONAL STYLE AND SCRIPT ABOVE#
